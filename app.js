92cc1d520fcbedcb7048b6e812dc58ff88c8f1ca
Kendallyn <Kendallynlyon@hotmail.com>
2017-08-11 09:32:37 -0700

    updated questions

--- app.js
+++ app.js
@@ -37,16 +37,16 @@ var quiz = [
         explanation: "Wolves are carnivores and rely on meat as their primary food source",
     },
     {
-        question: "How successful are wolves when they go to hunt?",
+        question: "Approximately how successful are wolves when they go to hunt?",
         choices: ["10% of the time", "25% of the time", "50% of the time", "60% of the time"],
         correct: 0,
-        explanation: "Wolves are only successful hunting 1 out of every 10 times, 10%",
+        explanation: "Wolves are successful in hunting approximately 1 out of every 10 times, 10%",
     },
     {
-        question: "What is the jaw power of a gray wolf in pounds per square inch?",
-        choices: ["500psi", "750psi", "1500psi", "2000psi"],
-        correct: 2,
-        explanation: "Wolves have 1500psi jaw power, compared to German Shepherds with 750psi",
+        question: "How much stronger is a wolf jaw compared to a domesticated dog?",
+        choices: ["About the same", "twice as strong", "five times stronger", "it is unknown"],
+        correct: 1,
+        explanation: "Wolves have twice as much jaw power, compared to domesicated dogs",
     },
     {
         question: "How many pounds of meat can wolves consume in a single feeding?",
@@ -58,7 +58,7 @@ var quiz = [
         question: "How many species of wolves are there in the world?",
         choices: ["1", "3", "5", "7"],
         correct: 1,
-        explanation: "There are 3 species of wolves in the world: Gray wolf, red wolf, and Ethiopian wolf",
+        explanation: "There are 3 species of wolves in the world: the gray wolf, the red wolf and the Ethiopian wolf",
     },
     {
         question: "Which of the following is a subspecies of the gray wolf?",
